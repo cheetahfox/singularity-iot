c06ceda4e5ef1e37e30301f4c6b786e742071e5d
Joshua Snyder <josh@cheetahfox.com>
2022-12-10 02:58:04 +0000

    Removed the binary to testenv

--- TestENV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export INFLUX_TOKEN="0isT56C3EHJwDxgve9cwjk7RbZjazXLbB8nIWf_OjEpl5I8M4CCsmujtJ7NGVvkt88EwbXcCajVrTz561x819g=="
-export INFLUX_SERVER="http://influxd.cheetahfox.com:8086/"
-export INFLUX_BUCKET="homeMonitoring-Dev"
-export INFLUX_ORG="home"
-export MQTT_BROKER="tcp://mqtt.cheetahfox.com:1883"
-export MQTT_ID="sing-dev"
-export MQTT_TOPIC="shellies/#"
-export DB_MAX_ERROR="10"
