885da5a8293f6dc16b86652762ccf2be29628092
Joshua Snyder <josh@cheetahfox.com>
2022-12-11 02:05:38 +0000

    Database health enhancement - still need to work on this

--- database/influx.go
+++ database/influx.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/cheetahfox/singularity-iot/config"
 	"github.com/cheetahfox/singularity-iot/health"
@@ -18,26 +20,52 @@ var dbclient influxdb2.Client
 func ConnectInflux(config config.Configuration) {
 	dbclient = influxdb2.NewClient(config.InfluxdbServer, config.Token)
 	dbhealth, err := dbclient.Health(context.Background())
-	if (err != nil) && dbhealth.Status == domain.HealthCheckStatusPass {
+	if (err != nil) && dbhealth.Status == domain.HealthCheckStatusFail {
 		log.Panic(err)
 	}
 	DbWrite = dbclient.WriteAPI(config.Org, config.Bucket)
 	errorsCh := DbWrite.Errors()
+
 	// Catch any write errors
 	go func() {
 		var errorCount int
 		for err := range errorsCh {
 			fmt.Printf("Influx write error: %s\n", err.Error())
 			errorCount++
-			// max number of errors before we set health state to false.
-			if errorCount > config.InfluxMaxError {
+
+			/*
+				check if the Influxdb database is healthy after seeing a error
+				I don't really like this logic I need to come up with something
+				simpler and more reliable.
+			*/
+			if !DbHealthCheck(time.Duration(errorCount) * time.Second) {
 				health.InfluxReady = false
-				fmt.Println("Maximum Influx error count reached!")
+				fmt.Println("unhealthy Influxdb")
+				if errorCount > config.InfluxMaxError {
+					fmt.Println("Maximum Influx error count reached!")
+					DisconnectInflux()
+					time.Sleep(time.Duration(5) * time.Second)
+					os.Exit(1)
+				}
+			} else {
+				// Reset error count if the database is healthy
+				errorCount = 0
+				health.InfluxReady = true
 			}
 		}
 	}()
 	fmt.Printf("Connected to Influxdb %s\n", config.InfluxdbServer)
 	health.InfluxReady = true
+}
+
+// Check health status after sleeping for a duration
+func DbHealthCheck(sleepTime time.Duration) bool {
+	time.Sleep(sleepTime)
+	dbhealth, err := dbclient.Health(context.Background())
+	if (err != nil) || dbhealth.Status == domain.HealthCheckStatusFail {
+		return false
+	}
+	return true
 }
 
 func DisconnectInflux() {
